a0899846692b9906434554dbf0470b6acac44b35
Justin Leahy <justinjleahy@gmail.com>
2024-03-03 14:20:27 -0500

    Removed unused import

--- src/main/java/com/justinleahy/dailyusage/EntryController.java
+++ src/main/java/com/justinleahy/dailyusage/EntryController.java
@@ -1,6 +1,5 @@
 package com.justinleahy.dailyusage;
 
-import com.justinleahy.dailyusage.Entry;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
